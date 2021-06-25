9e1d5c503527e8dae1448f782c01fedb125ff7e6
mahanghang <mahanghang@bytedance.com>
2021-06-25 17:11:52 +0800

    add bn

--- model/EmbeddingMLP.py
+++ model/EmbeddingMLP.py
@@ -12,8 +12,10 @@ class EmbeddingMLP(nn.Module):
         for i in range(len(categorial_feature_vocabsize)):
             self.embedding_layer_list.append(nn.Embedding(categorial_feature_vocabsize[i], embed_dim))
         self.mlp1 = nn.Linear(len(categorial_feature_vocabsize)*embed_dim+len(continous_feature_names), hidden_dim[0])
+        self.bn1 = nn.BatchNorm1d(hidden_dim[0])
         self.relu1 = nn.ReLU(inplace=True)
         self.mlp2 = nn.Linear(hidden_dim[0], hidden_dim[1])
+        self.bn2 = nn.BatchNorm1d(hidden_dim[1])
         self.relu2 = nn.ReLU(inplace=True)
         self.mlp3 = nn.Linear(hidden_dim[1], 1)
         self.act = nn.Sigmoid()
@@ -25,7 +27,7 @@ class EmbeddingMLP(nn.Module):
             embed_out_list.append(embed_out)
         xv = torch.cat(embed_out_list, dim=1)
         x = torch.cat((xi, xv), dim=1)
-        x = self.relu1(self.mlp1(x))
-        x = self.relu2(self.mlp2(x))
+        x = self.relu1(self.bn1(self.mlp1(x)))
+        x = self.relu2(self.bn2(self.mlp2(x)))
         x = self.act(self.mlp3(x))
         return x
